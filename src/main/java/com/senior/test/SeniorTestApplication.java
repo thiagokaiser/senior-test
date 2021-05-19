85379698e45d6d84cdc6c9d6caefb0a70aa17690
Thiago Kaiser <thiago.kaiser@hotmail.com>
2021-05-19 10:40:39 -0300

    projeto criado

---
+++ src/main/java/com/senior/test/SeniorTestApplication.java
@@ -0,0 +1,13 @@
+package com.senior.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senior/test/SeniorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senior.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
