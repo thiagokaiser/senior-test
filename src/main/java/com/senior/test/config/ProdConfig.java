d23b140b03343bcd8182b962772c4fe1a79ad850
Thiago Kaiser <thiago.kaiser@hotmail.com>
2021-05-20 17:40:46 -0300

    Configurado PostgreSQL

--- src/main/java/com/senior/test/config/ProdConfig.java
+++ src/main/java/com/senior/test/config/ProdConfig.java
@@ -1,5 +1,10 @@
 package com.senior.test.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
 public class ProdConfig {
 
 }
